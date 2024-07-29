7adc1360174b116aff5a228b8b78bfc635d9cdfe
Geon Kim <geon6757@kaist.ac.kr>
2024-07-29 16:14:09 +0900

    Remove: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import React from 'react';
 
 import './App.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
 
   return (
     <>
